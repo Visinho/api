b9026ed87e51bf1818771e027f71c82663ad2b5f
Visinho <Elvisvisinho@rocketmail.com>
2022-09-12 19:46:02 +0100

    Models and routes

---
+++ api/routes/auth.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+    try{
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+        const user = await newUser.save();
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
